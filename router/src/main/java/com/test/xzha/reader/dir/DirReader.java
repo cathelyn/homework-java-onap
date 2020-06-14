496341e2de6cf2b53afb846927f5c8cfe119749b
cathelyn <bulkova.katarina@gmail.com>
2020-06-14 18:53:39 +0200

    minimal viable product

--- router/src/main/java/com/test/xzha/reader/dir/DirReader.java
+++ router/src/main/java/com/test/xzha/reader/dir/DirReader.java
@@ -2,11 +2,39 @@ package com.test.xzha.reader.dir;
 
 import org.apache.log4j.Logger;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.stream.Collectors;
 
 public class DirReader {
 	private static final Logger LOG = Logger.getLogger(DirReader.class);
+	private ExecutorService executor;
+
+
+	public void addTree(File file, Collection<File> allFiles) {
+
+		File[] children = file.listFiles();
+		if (children != null) {
+			for (File child : children) {
+				allFiles.add(child);
+				addTree(child, allFiles);
+			}
+		}
+	}
+
+
+	public Collection<File> searchFiles(String dirPath) throws IOException {
+
+		Collection<File> allFiles = new ArrayList<File>();
+		addTree(new File(dirPath), allFiles);
+
+		return allFiles;
+
+	}
+
 
 	/**
 	 * Find all prefixes matched to phone
@@ -18,19 +46,36 @@ public class DirReader {
 	public List<String[]> searchPrefixes(String dirPath, String phone) throws IOException {
 
 		// put your implementation here, please
-		//metoda na rekurzivne ziskanie vsetkych paths -> ziskam vsetky subory na porovnavanie
-		for
-		//kazdy subor zoradim od najdlhsieho po najkratsi
-		// v kazdom subore vratim prvy mozny vyskyt do totalMatches
+		String[] entry = new String[3];
+		List<String[]> totalMatches = new ArrayList<>();
+		Collection<File> files = searchFiles(dirPath);
+		files = files.stream().filter(File::isFile).collect(Collectors.toList());
+
+		for (File file : files) {
+			try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+				String line;
+				while ((line = br.readLine()) != null) {
+					if (line.length() > 0 && phone.startsWith(line.split("\\s+")[0])) {
+						entry[0] = line.split("\\s+")[0];
+						entry[1] = line.split("\\s+")[1];
+						entry[2] = file.getName();
+						totalMatches.add(entry);
+					}
+				}
+
+			} catch (IOError e) {
+				System.err.println("File reading error occurred.");
+			}
+		}
 
 		return totalMatches;
-    }
+	}
 
-    /**
-     * Shutdown ExecutorService
-     */
+	/**
+	 * Shutdown ExecutorService
+	 */
 	public void shutdown(){
-        executor.shutdown();
-    }
+		executor.shutdown();
+	}
 
 }
